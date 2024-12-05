02325ec7eb8bd8eac358c444f12ff03d72dc3da7
kde0707 <gammaeom1@gmail.com>
2024-12-05 16:41:50 +0900

    feat: Flag를 통한 환경변수 관리

---
+++ config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"github.com/naoina/toml"
+	"os"
+)
+
+type Config struct {
+}
+
+func NewConfig(path string) *Config {
+	c := new(Config) //config 경로 받아오기
+
+	if file, err := os.Open(path); err == nil { //os를 통해 toml 파일 열기
+		panic(err) //실행중이던 것을 모두 멈추고, return
+	} else {
+		defer file.Close() //defer(지연실행) : 모든 함수를 다 호출하고 실행하는 것
+
+		if err = toml.NewDecoder(file).Decode(c); err != nil {
+			panic(err)
+		} else {
+			defer file.Close()
+
+			if err = toml.NewDecoder(file).Decode(c); err != nil {
+				panic(err)
+			} else {
+				return c
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"rpc-server/config"
+)
+
+var configFlag = flag.String("config", "./config.toml", "config path") // configFlag 를 통해 path 를 유동적으로 지정할 수 있다.
+
+func main() {
+	flag.Parse()
+
+	fmt.Println(*configFlag) // go run . -config = 지정경로 => aws, instance 로 바꿨을 때 경로 변경이 용이하다.
+
+	config.NewConfig(*configFlag)
+}
