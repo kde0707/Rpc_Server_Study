e6eaff4a638378aea1fe332a420c479c0deff9d5
kde0707 <gammaeom1@gmail.com>
2024-12-06 14:53:39 +0900

    feat: gRPC Client 연결
     - client 연결

--- gRPC/client/client.go
+++ gRPC/client/client.go
@@ -1,11 +1,13 @@
 package client
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"rpc-server/config"
 	"rpc-server/gRPC/paseto"
 	auth "rpc-server/gRPC/proto"
+	"time"
 )
 
 type GRPCClient struct {
@@ -17,7 +19,7 @@ type GRPCClient struct {
 func NewGRPCClient(cfg *config.Config) (*GRPCClient, error) {
 	c := new(GRPCClient)
 
-	if client, err := grpc.Dial(cfg.GRPC.URL, grpc.WithTransportCredentials(insecure.NewCredentials())); err != nil {
+	if client, err := grpc.Dial(cfg.GRPC.URL, grpc.WithTransportCredentials(insecure.NewCredentials())); err != nil { //WithTransportCredentials 보안 연결을 사용하지 않는 경우
 		return nil, err
 	} else {
 		c.client = client
@@ -29,10 +31,35 @@ func NewGRPCClient(cfg *config.Config) (*GRPCClient, error) {
 	return c, nil
 }
 
-func (g *GRPCClient) CreateAuth(address string) (*auth.AuthData, error) {
+func (g *GRPCClient) CreateAuth(name string) (*auth.AuthData, error) {
+	now := time.Now()
+	expiredTime := now.Add(30 * time.Minute)
+
+	a := &auth.AuthData{
+		Name:       name,
+		CreateDate: now.Unix(),
+		ExpireDate: expiredTime.Unix(),
+	}
+
+	if token, err := g.pasetoMaker.CreateNewToken(a); err != nil {
+		return nil, err
+	} else {
+		a.Token = token
+
+		if res, err := g.authClient.CreateAuth(context.Background(), &auth.CreateTokenReq{Auth: a}); err != nil {
+			return nil, err
+		} else {
+			return res.Auth, nil
+		}
+	}
 	return nil, nil
 }
 
-func (g *GRPCClient) VerifyAuth(token string) (*auth.VerifyTokenRes, error) {
+func (g *GRPCClient) VerifyAuth(token string) (*auth.Verify, error) {
+	if res, err := g.authClient.VerifyToken(context.Background(), &auth.VerifyTokenReq{Token: token}); err != nil {
+		return nil, err
+	} else {
+		return res.V, nil
+	}
 	return nil, nil
 }
