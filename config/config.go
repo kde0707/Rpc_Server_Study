9d76be9cad0aaa4987714d2a5b5b072dec5104a6
kde0707 <gammaeom1@gmail.com>
2024-12-06 09:56:41 +0900

    feat: 서버 구동
     - StartServer 구현
     - config.go 버그 수정

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ type Config struct {
 func NewConfig(path string) *Config {
 	c := new(Config) //config 경로 받아오기
 
-	if file, err := os.Open(path); err == nil { //os를 통해 toml 파일 열기
+	if file, err := os.Open(path); err != nil { //os를 통해 toml 파일 열기
 		panic(err) //실행중이던 것을 모두 멈추고, return
 	} else {
 		defer file.Close() //defer(지연실행) : 모든 함수를 다 호출하고 실행하는 것
--- network/router.go
+++ network/router.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"github.com/gin-gonic/gin"
 	"rpc-server/config"
 	"rpc-server/service"
 )
@@ -8,9 +9,15 @@ import (
 type Network struct {
 	cfg     *config.Config
 	service *service.Service
+	engin   *gin.Engine
 }
 
 func NewNetwork(cfg *config.Config, service *service.Service) (*Network, error) {
-	r := &Network{cfg: cfg, service: service}
+	r := &Network{cfg: cfg, service: service, engin: gin.New()}
+
 	return r, nil
+}
+
+func (n *Network) StartServer() {
+	n.engin.Run(":8080")
 }
