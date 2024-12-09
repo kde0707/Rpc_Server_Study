9221fdf25229615aa65521210d294ee2d9791db6
kde0707 <gammaeom1@gmail.com>
2024-12-09 09:50:59 +0900

    feat: http 라우터와 grpc 연결
     - http, grpc 연결

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"rpc-server/cmd"
 	"rpc-server/config"
+	"rpc-server/gRPC/server"
+	"time"
 )
 
 var configFlag = flag.String("config", "./config.toml", "config path") // configFlag 를 통해 path 를 유동적으로 지정할 수 있다.
@@ -13,5 +15,10 @@ func main() {
 	//fmt.Println(*configFlag) // go run . -config = 지정경로 => aws, instance 로 바꿨을 때 경로 변경이 용이하다.
 	cfg := config.NewConfig(*configFlag)
 
-	cmd.NewApp(cfg)
+	if err := server.NewGRPCServer(cfg); err != nil {
+		panic(err)
+	} else {
+		time.Sleep(1e9)
+		cmd.NewApp(cfg)
+	}
 }
--- network/router.go
+++ network/router.go
@@ -3,17 +3,19 @@ package network
 import (
 	"github.com/gin-gonic/gin"
 	"rpc-server/config"
+	"rpc-server/gRPC/client"
 	"rpc-server/service"
 )
 
 type Network struct {
-	cfg     *config.Config
-	service *service.Service
-	engin   *gin.Engine
+	cfg        *config.Config
+	service    *service.Service
+	gRPCClient *client.GRPCClient
+	engin      *gin.Engine
 }
 
-func NewNetwork(cfg *config.Config, service *service.Service) (*Network, error) {
-	r := &Network{cfg: cfg, service: service, engin: gin.New()}
+func NewNetwork(cfg *config.Config, service *service.Service, gRPCClient *client.GRPCClient) (*Network, error) {
+	r := &Network{cfg: cfg, service: service, engin: gin.New(), gRPCClient: gRPCClient}
 
 	return r, nil
 }
